b7ec58a9e3052a71fef46734edb5b508b5c66a55
Keith Damiani <keith@tighten.co>
2018-01-31 15:32:17 -0500

    Alias new classes

--- src/Tightenco/Support/alias.php
+++ src/Tightenco/Support/alias.php
@@ -1,12 +1,31 @@
 <?php
 
 $aliases = [
+    /**
+     * Contracts
+     */
     Tightenco\Contracts\Support\Arrayable::class => Illuminate\Contracts\Support\Arrayable::class,
+    Tightenco\Contracts\Support\Htmlable::class => Illuminate\Contracts\Support\Htmlable::class,
     Tightenco\Contracts\Support\Jsonable::class => Illuminate\Contracts\Support\Jsonable::class,
-    Tightenco\Support\Traits\Macroable::class => Illuminate\Support\Traits\Macroable::class,
+
+    /**
+     * Support
+     */
     Tightenco\Support\Arr::class => Illuminate\Support\Arr::class,
     Tightenco\Support\Collection::class => Illuminate\Support\Collection::class,
     Tightenco\Support\HigherOrderCollectionProxy::class => Illuminate\Support\HigherOrderCollectionProxy::class,
+    Tightenco\Support\HtmlString::class => Illuminate\Support\HtmlString::class,
+
+    /**
+     * Support\Debug
+     */
+    Tightenco\Support\Debug\Dumper::class => Illuminate\Support\Debug\Dumper::class,
+    Tightenco\Support\Debug\HtmlDumper::class => Illuminate\Support\Debug\HtmlDumper::class,
+
+    /**
+     * Support\Traits
+     */
+    Tightenco\Support\Traits\Macroable::class => Illuminate\Support\Traits\Macroable::class,
 ];
 
 foreach ($aliases as $tighten => $illuminate) {
