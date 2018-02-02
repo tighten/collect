bc71849f7b3b9222d3a9dce631671ecc30c222c2
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-02-02 00:43:46 -0200

    Revert back to @damiani's version

--- stubs/src/Collect/Support/alias.php
+++ stubs/src/Collect/Support/alias.php
@@ -2,8 +2,8 @@
 
 $aliases = [/*--- ALIASES ---*/];
 
-foreach ($aliases as $illuminate => $tighten) {
-    if (class_exists($illuminate) && ! interface_exists($illuminate) && ! trait_exists($illuminate)) {
-        class_alias($illuminate, $tighten);
+foreach ($aliases as $tighten => $illuminate) {
+    if (! class_exists($illuminate) && ! interface_exists($illuminate) && ! trait_exists($illuminate)) {
+        class_alias($tighten, $illuminate);
     }
 }
