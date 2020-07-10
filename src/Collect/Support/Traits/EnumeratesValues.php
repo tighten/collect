e0fff76f51bb3693a495fc6b0c53d4cefb71f8fb
Jamison Valenta <jamison@tigten.co>
2020-07-10 15:42:34 -0600

    Larave 7.4.0 changes

--- src/Collect/Support/Traits/EnumeratesValues.php
+++ src/Collect/Support/Traits/EnumeratesValues.php
@@ -10,6 +10,7 @@ use Tightenco\Collect\Support\Arr;
 use Tightenco\Collect\Support\Collection;
 use Tightenco\Collect\Support\Enumerable;
 use Tightenco\Collect\Support\HigherOrderCollectionProxy;
+use Tightenco\Collect\Support\HigherOrderWhenProxy;
 use JsonSerializable;
 use Symfony\Component\VarDumper\VarDumper;
 use Traversable;
@@ -404,12 +405,16 @@ trait EnumeratesValues
      * Apply the callback if the value is truthy.
      *
      * @param  bool|mixed  $value
-     * @param  callable  $callback
+     * @param  callable|null  $callback
      * @param  callable|null  $default
      * @return static|mixed
      */
-    public function when($value, callable $callback, callable $default = null)
+    public function when($value, callable $callback = null, callable $default = null)
     {
+        if (! $callback) {
+            return new HigherOrderWhenProxy($this, $value);
+        }
+
         if ($value) {
             return $callback($this, $value);
         } elseif ($default) {
